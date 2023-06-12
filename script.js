1eb3cfc000f1f62b2c8a7607f91dcd9583238758
YaKuBoVO <oyakubov526@gmail.com>
2023-06-12 17:45:44 +0500

    pashti tayyor

---
+++ script.js
@@ -0,0 +1,94 @@
+"use strict"
+
+
+let contacts = [
+   {
+      id: " ",
+      name: "John Smith",
+      email: "john2432@gmail.com",
+      phone: "123-456-789",
+      delete: " ",
+   },
+   {
+      id: " ",
+      name: "Otabek Y",
+      email: "yakubov@gmail.com",
+      phone: "333-444-555",
+      delete: " ",
+   },
+   {
+      id: " ",
+      name: "Murodov Susambil",
+      email: "susambil123@gmail.com",
+      phone: "123-555--444",
+      delete: " ",
+   },
+];
+
+
+let tableBody = document.getElementById("contact-table-body");
+let count = 1;
+let del = "x";
+
+
+
+function showTableBody(arr) {
+   tableBody.innerHTML = " ";
+   for (let i = 0; i < arr.length; i++) {
+      let contact = arr[i];
+   
+      let row = document.createElement("tr");
+
+      let idCell = document.createElement("td");
+      idCell.textContent = count;
+      row.appendChild(idCell);
+   
+      let nameCell = document.createElement("td");
+      nameCell.textContent = contact.name;
+      row.appendChild(nameCell);
+   
+   
+      let emailCell = document.createElement("td");
+      emailCell.textContent = contact.email;
+      row.appendChild(emailCell);
+   
+   
+      let phoneCell = document.createElement("td");
+      phoneCell.textContent = contact.phone;
+      row.appendChild(phoneCell);
+   
+      let deleteCell = document.createElement("td");
+      deleteCell.textContent = del;
+      deleteCell.addEventListener('click', () => {
+         row.remove();
+      });
+      tableBody.appendChild(row);
+      count++;
+   }
+}
+
+showTableBody(contacts);
+
+let contactForm = document.getElementById("contact-form");
+contactForm.addEventListener("submit", function(e) {
+   e.preventDefault();
+
+   let nameInput = document.getElementById("name");
+   let emailInput = document.getElementById("email");
+   let phoneInput = document.getElementById("phone");
+
+
+   let newContact = {
+      id: 0,
+      name:  nameInput.value,
+      email: emailInput.value,
+      phone: phoneInput.value,
+   };
+   contacts.push(newContact);
+
+   showTableBody(contacts);
+
+   nameInput.value = "";
+   emailInput.value = "";
+   phoneInput.value = "";
+});
